bb999e1af3b2d55c6d52b0f4bd44c3105cd2d4fd
Je-yeong Song <46372718+hooreique@users.noreply.github.com>
2023-06-10 11:44:40 +0900

    back-end/chore: Add controller

--- back-end/src/main/java/com/example/demo/forum/freetalk/web/FreeTalkController.java
+++ back-end/src/main/java/com/example/demo/forum/freetalk/web/FreeTalkController.java
@@ -1,7 +1,42 @@
 package com.example.demo.forum.freetalk.web;
 
+import com.example.demo.forum.freetalk.repository.FreeTalkCommentRepository;
+import com.example.demo.forum.freetalk.repository.FreeTalkPostRepository;
+import com.example.demo.member.repository.MemberRepository;
+import com.google.common.collect.ImmutableMap;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api/free-talk")
+@RequiredArgsConstructor
 public class FreeTalkController {
+
+    private final MemberRepository memberRepository;
+
+    private final FreeTalkPostRepository postRepository;
+
+    private final FreeTalkCommentRepository commentRepository;
+
+    @GetMapping
+    @Transactional(readOnly = true)
+    public Page<Map<String, Object>> page(
+            final @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return postRepository.findAll(pageable)
+                .map(post -> new ImmutableMap.Builder<String, Object>()
+                        .put("id", post.getId())
+                        .put("authorId", post.getAuthor().getId())
+                        .put("authorName", post.getAuthor().getName())
+                        .put("title", post.getTitle())
+                        .build());
+    }
 }
