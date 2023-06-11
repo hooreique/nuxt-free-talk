95dcb45fdd9d32c2c8db466f1e845fa33a56227e
Je-yeong Song <46372718+hooreique@users.noreply.github.com>
2023-06-11 16:22:34 +0900

    chore: Add run config for idea

--- middle-dev/server.mjs
+++ middle-dev/server.mjs
@@ -1,23 +1,23 @@
-import { log } from "node:console";
+import {log} from "node:console";
 import express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import {createProxyMiddleware} from "http-proxy-middleware";
 
 const app = express();
 
 app.use(
-  "/api",
-  createProxyMiddleware({
-    target: "http://127.0.0.1:8080",
-  })
+    "/api",
+    createProxyMiddleware({
+        target: "http://127.0.0.1:8080",
+    })
 );
 
 app.use(
-  "/",
-  createProxyMiddleware({
-    target: "http://127.0.0.1:3000",
-  })
+    "/",
+    createProxyMiddleware({
+        target: "http://127.0.0.1:3000",
+    })
 );
 
 app.listen(3333, () => {
-  log("Proxy server is running on port 3333");
+    log("Proxy server is running on port 3333");
 });
